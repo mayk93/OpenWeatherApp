16b170353abc19845dd2ef4ced9c95de673ccef1
michael <mihai@mandrescu.co>
2017-10-29 18:01:35 +0000

    Added service scripts for API server.

--- backend/api_server/start_server.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-uwsgi --chmod-socket=666 --uwsgi-socket /home/michael/server/backend.sock --wsgi-file /home/michael/GitHub/OpenWeatherApp/backend/api_server/api_server.py --callable __hug_wsgi__
