b1e3a9c6e9900aaf0b29ef22fb7c57b4ea42239f
michael <mihai@mandrescu.co>
2018-03-05 07:54:27 +0000

    Minor change in open weather app.

--- backend/api_server/start_open_weather_server.sh
+++ backend/api_server/start_open_weather_server.sh
@@ -4,6 +4,6 @@ touch /tmp/open_weather_server.pid
 
 export PROD="true"
 
-uwsgi --chmod-socket=666 --uwsgi-socket /home/deploy/server/open_weather_server.sock --wsgi-file /home/deploy/OpenWeatherApp/backend/api_server/api_server.py --callable __hug_wsgi__ &
+uwsgi --chmod-socket=666 --uwsgi-socket /home/deploy/open_weather_server/open_weather_server.sock --wsgi-file /home/deploy/OpenWeatherApp/backend/api_server/api_server.py --callable __hug_wsgi__ &
 
 echo $! > /tmp/open_weather_server.pid
