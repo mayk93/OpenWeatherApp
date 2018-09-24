509fd13fd9773208d1eee46a6ff44de0bd16cf28
michael <mihai@mandrescu.co>
2018-09-24 22:38:14 +0100

    Downgrade to http

--- backend/api_server/constants.py
+++ backend/api_server/constants.py
@@ -2,5 +2,5 @@ import os
 
 PROD = os.environ.get("PROD") == "true"
 
-ALLOWED_ORIGINS = ["https://projects.myapps.gallery"] if PROD else ["*"]  # http://localhost:3000
+ALLOWED_ORIGINS = ["http://projects.myapps.gallery"] if PROD else ["*"]  # http://localhost:3000
 KEY_LOCATIONS = ["keys.json", "/home/deploy/server/keys.json"]
